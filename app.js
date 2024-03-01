f5c051ab507cd54afe837a1ce13a3257511e1a47
Nathanael Sumpter <nsumpter98@gmail.com>
2024-03-01 13:32:31 -0500

    cors

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 80
+const express = require('express');
+const cors = require('cors'); // Import the cors package
+const app = express();
+const port = 80;
+
+// Use the cors middleware
+app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
